b953669d000bad220482d8d545a1255da449a937
wrkn <wrknpy@gmail.com>
2023-11-03 12:41:14 -0600

    added menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title('My Parents New Healthier Diner')
+st.header('Breakfast menu')
+st.text('Omege 3 and Bluberry Oatmeal')
+st.text('Kale, Spinach and Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
