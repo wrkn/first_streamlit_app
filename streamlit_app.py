e61cdf49c281c989a70a7751e9e428f5911f39bf
wrkn <wrknpy@gmail.com>
2023-11-03 12:58:39 -0600

    picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ st.text('Kale, Spinach and Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 st.dataframe(my_fruit_list)
 
