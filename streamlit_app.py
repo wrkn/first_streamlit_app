4c5349d2fe4f1173ff02c950f3a8a977f9abc1d0
wrkn <wrknpy@gmail.com>
2023-11-03 12:47:15 -0600

    fix typo pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ st.text('Omege 3 and Bluberry Oatmeal')
 st.text('Kale, Spinach and Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
