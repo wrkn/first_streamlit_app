b9edc001ca39ac159eeda297f5363dfc3fe2a9c1
wrkn <wrknpy@gmail.com>
2023-11-03 12:59:17 -0600

    picker2

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 st.dataframe(my_fruit_list)
 
