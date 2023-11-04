c00346efd3de22fb1c3b56939ffdb975e0bcd33e
wrkn <wrknpy@gmail.com>
2023-11-03 22:13:01 -0600

    fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
