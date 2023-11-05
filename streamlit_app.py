60051840bc3dca7f74706b7e6da8dc744e022322
wrkn <wrknpy@gmail.com>
2023-11-04 18:23:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #st.dataframe(fruits_to_show)
 
-st.header('Fruityvice Fruit Advice!)
+st.header('Fruityvice Fruit Advice!')
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
